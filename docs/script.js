f3cc17b0cc19d7c5594d312a44b55814a84c476c
unknown <edesp.lopez17@gmail.com>
2024-08-08 12:52:25 -0700

    user is able to view forecast for 3 days

--- docs/script.js
+++ docs/script.js
@@ -11,7 +11,7 @@ $(function () {
 
 function fetchWeather(cityName) {
   var apiKey = "0ae6980ca849403b9c003014241103";
-  var apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&days=5&aqi=no&alerts=no&q=" + cityName; //builds the url to display the weather. city name is chosen then displays said citynames location and weather information
+  var apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=" + cityName + "&days=5&aqi=no&alerts=no"; //builds the url to display the weather. city name is chosen then displays said citynames location and weather information
   
   $.ajax({
       url: apiUrl, //gets the desired url when chosing a city to display correct data
@@ -22,11 +22,11 @@ function fetchWeather(cityName) {
   });
 }
 
-function displayWeather(weatherData) { //gathers weather data from the fetchweather function on line 12
+function displayWeather(weatherData) {
   var currentWeather = weatherData.current;
   var forecast = weatherData.forecast.forecastday;
 
-  //displays the same-day weather
+  // Displays the current weather
   var currentWeatherInfo = "<h2>Current Weather for " + weatherData.location.name + "</h2>";
   currentWeatherInfo += "<div class='current-weather'>";
   currentWeatherInfo += "<p>Temperature: " + currentWeather.temp_f + "°F</p>";
@@ -34,19 +34,20 @@ function displayWeather(weatherData) { //gathers weather data from the fetchweat
   currentWeatherInfo += "<img src='" + currentWeather.condition.icon + "' alt='Weather icon'>";
   currentWeatherInfo += "</div>";
 
-  //displays the 5 day forecast
-  var forecastInfo = "<h2>3-Day Weather Forecast</h2>";
-    for (var i = 0; i < forecast.length; i++) {
-        var day = forecast[i];
-        forecastInfo += "<div class='forecast-day'>";
-        forecastInfo += "<h3>" + day.date + "</h3>";
-        forecastInfo += "<p>Max Temp: " + day.day.maxtemp_f + "°F</p>";
-        forecastInfo += "<p>Min Temp: " + day.day.mintemp_f + "°F</p>";
-        forecastInfo += "<p>Condition: " + day.day.condition.text + "</p>";
-        forecastInfo += "<img src='" + day.day.condition.icon + "' alt='Weather icon'>";
-        forecastInfo += "</div>";
-    }
-    //html content
+  // Displays the 5-day forecast
+  var forecastInfo = "<h2>5-Day Weather Forecast</h2>";
+  for (var i = 0; i < forecast.length; i++) {
+    var day = forecast[i];
+    forecastInfo += "<div class='forecast-day'>";
+    forecastInfo += "<h3>" + day.date + "</h3>";
+    forecastInfo += "<p>Max Temp: " + day.day.maxtemp_f + "°F</p>";
+    forecastInfo += "<p>Min Temp: " + day.day.mintemp_f + "°F</p>";
+    forecastInfo += "<p>Condition: " + day.day.condition.text + "</p>";
+    forecastInfo += "<img src='" + day.day.condition.icon + "' alt='Weather icon'>";
+    forecastInfo += "</div>";
+  }
+
+  // Updates the HTML content
   $("#currentWeather").html(currentWeatherInfo);
   $("#weatherData").html(forecastInfo); //selects the weatherdata id in the html to display the weatherData text
 }
