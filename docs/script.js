c06a1b45a56fa26d095cfda97f17502d14e3b5c8
unknown <edesp.lopez17@gmail.com>
2024-08-09 12:09:39 -0700

    made a new api now app displayed 5 days instead of 3

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,25 @@
 $(function () {
-  var cityNames = ["Salem", "Portland", "Woodburn", "Eugene", "Oregon City", "Wilsonville"]; //lists of available cities
-  $('#citySearch').autocomplete({ //autocomplete widget for chosen city names
-      source: cityNames,
-      select: function(event, ui) {
-          var cityName = ui.item.value;
-          fetchWeather(cityName);
-      }
+  var cityData = {
+    "Salem": "97305",
+    "Portland": "97201",
+    "Woodburn": "97071",
+    "Eugene": "97401",
+    "Oregon City": "97045",
+    "Wilsonville": "97070"
+  };
+
+  $('#citySearch').autocomplete({
+    source: Object.keys(cityData),
+    select: function(event, ui) {
+      var cityName = ui.item.value;
+      fetchWeather(cityName);
+    }
   });
 });
 
 function fetchWeather(cityName) {
-  var apiKey = "0ae6980ca849403b9c003014241103";
-  var apiUrl = "https://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=" + cityName + "&days=5&aqi=no&alerts=no"; //builds the url to display the weather. city name is chosen then displays said citynames location and weather information
+  var apiKey = "5dc3500fd1c6fa3d7526f8d1b2b748e9";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&cnt=5&appid=" + apiKey; //builds the url to display the weather. city name is chosen then displays said citynames location and weather information
   
   $.ajax({
       url: apiUrl, //gets the desired url when chosing a city to display correct data
@@ -23,27 +31,27 @@ function fetchWeather(cityName) {
 }
 
 function displayWeather(weatherData) {
-  var currentWeather = weatherData.current;
-  var forecast = weatherData.forecast.forecastday;
+  // var currentWeather = weatherData.current;
+  // var forecast = weatherData.forecast.forecastday;
 
   // Displays the current weather
-  var currentWeatherInfo = "<h2>Current Weather for " + weatherData.location.name + "</h2>";
+  var currentWeatherInfo = "<h2>Current Weather for " + weatherData.city.name + "</h2>";
   currentWeatherInfo += "<div class='current-weather'>";
-  currentWeatherInfo += "<p>Temperature: " + currentWeather.temp_f + "°F</p>";
-  currentWeatherInfo += "<p>Condition: " + currentWeather.condition.text + "</p>";
-  currentWeatherInfo += "<img src='" + currentWeather.condition.icon + "' alt='Weather icon'>";
+  currentWeatherInfo += "<p>Temperature: " + weatherData.list[0].main.temp + "°F</p>";
+  currentWeatherInfo += "<p>Condition: " + weatherData.list[0].weather[0].description + "</p>";
+  currentWeatherInfo += "<img src='https://openweathermap.org/img/wn/" + weatherData.list[0].weather[0].icon + "@2x.png' alt='Weather icon'>";
   currentWeatherInfo += "</div>";
 
   // Displays the 5-day forecast
   var forecastInfo = "<h2>5-Day Weather Forecast</h2>";
-  for (var i = 0; i < forecast.length; i++) {
-    var day = forecast[i];
+  for (var i = 0; i < weatherData.list.length; i++) {
+    var day = weatherData.list[i];
     forecastInfo += "<div class='forecast-day'>";
-    forecastInfo += "<h3>" + day.date + "</h3>";
-    forecastInfo += "<p>Max Temp: " + day.day.maxtemp_f + "°F</p>";
-    forecastInfo += "<p>Min Temp: " + day.day.mintemp_f + "°F</p>";
-    forecastInfo += "<p>Condition: " + day.day.condition.text + "</p>";
-    forecastInfo += "<img src='" + day.day.condition.icon + "' alt='Weather icon'>";
+    forecastInfo += "<h3>" + new Date(day.dt_txt).toLocaleDateString() + "</h3>";
+    forecastInfo += "<p>Max Temp: " + day.main.temp_max + "°F</p>";
+    forecastInfo += "<p>Min Temp: " + day.main.temp_min + "°F</p>";
+    forecastInfo += "<p>Condition: " + day.weather[0].description + "</p>";
+    forecastInfo += "<img src='https://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png' alt='Weather icon'>";
     forecastInfo += "</div>";
   }
 
